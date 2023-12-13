8a679d97bad879d5e47e0471384cf8fc9e061b1e
diniles <dzliashko@gmail.com>
2023-12-13 12:46:17 +0300

    Init: seminar 3 task1 class Student

---
+++ src/test/java/hw3/task1/Student.java
@@ -0,0 +1,48 @@
+package hw3.task1;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final String name;
+    private int age;
+    private transient double GPA;
+
+    public Student(String name, int age, double GPA) {
+        this.name = name;
+        this.age = age;
+        this.GPA = GPA;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", GPA=" + GPA +
+                '}';
+    }
+}
