d8a8e75be00cbdc23c4c3817b60d468abfcb5f28
diniles <dzliashko@gmail.com>
2023-12-13 13:00:36 +0300

    Feat: seminar 3 task2 class Student

---
+++ src/test/java/hw3/task2/Student.java
@@ -0,0 +1,56 @@
+package hw3.task2;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Student {
+    private String name;
+    private int age;
+    private transient double GPA;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, double GPA) {
+        this.name = name;
+        this.age = age;
+        this.GPA = GPA;
+    }
+
+    @XmlElement
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlElement
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @XmlElement
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", GPA=" + GPA +
+                '}';
+    }
+}
