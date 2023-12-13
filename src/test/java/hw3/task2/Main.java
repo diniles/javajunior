dfeadbaa4f9a37e07629e323afcbd0ff34924926
diniles <dzliashko@gmail.com>
2023-12-13 13:26:19 +0300

    Feat: seminar 3 task2 class Main

---
+++ src/test/java/hw3/task2/Main.java
@@ -0,0 +1,25 @@
+package hw3.task2;
+
+import static hw3.task2.StudentSerialization.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Markus", 25, 8.7);
+
+        System.out.println("Original class student");
+        System.out.println(student);
+        System.out.println();
+
+        serializeToJson(student, "student.json");
+        Student deserializedStudentJson = deserializeFromJson("student.json");
+        System.out.println("Deserialized from JSON class student");
+        System.out.println(deserializedStudentJson);
+        System.out.println();
+
+        serializeToXml(student, "student.xml");
+        Student deserializedStudentXml = deserializeFromJson("student.xml");
+        System.out.println("Deserialized from XML class student");
+        System.out.println(deserializedStudentXml);
+        System.out.println();
+    }
+}
